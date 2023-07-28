c8e6f93ff2df5c060c5aadaadeeb16e3413f4847
xiaobian233 <1282845084@qq.com>
2023-07-28 09:40:25 +0800

    feat: koa2添加

--- src/router/404.js
+++
@@ -1,7 +0,0 @@
-const handler404 = (res) => {
-  res.writeHead(404, { "Content-type": "text/plain" });
-  res.write("404 Not Found \n");
-  res.end('go404.index');
-};
-
-module.exports = handler404;
